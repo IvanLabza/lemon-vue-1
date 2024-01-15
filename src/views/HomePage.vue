d8970169183f7d53209d54261e76a6f1f6e644da
IvanLabza <vanalabza77@gmail.com>
2024-01-15 23:16:52 +0200

    main

---
+++ src/views/HomePage.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="container">
+		<HeaderBlock />
+	</div>
+</template>
+
+<script>
+import HeaderBlock from "@/components/HeaderBlock/HeaderBlock.vue";
+
+export default {
+	name: "HomePage",
+	data() {
+		return {
+			isChecked: false,
+		};
+	},
+	methods: {},
+	components: { HeaderBlock },
+};
+</script>
